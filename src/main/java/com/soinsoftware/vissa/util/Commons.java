335f4001d5afcd5aca93a7d90947eab91abd29bf
florli319 <florli319@gmail.com>
2019-01-04 02:53:40 -0500

    class for constants

---
+++ src/main/java/com/soinsoftware/vissa/util/Commons.java
@@ -0,0 +1,12 @@
+package com.soinsoftware.vissa.util;
+
+public class Commons {
+
+	public static String DOCUMENT_TYPE;
+	public static final String FORMAT_DATE = "dd/MM/yyyy HH:mm:ss";
+
+	public String getDocumentType() {
+		return DOCUMENT_TYPE;
+	}
+
+}
