329b53a90d3776a08b1c69cb44cf2dec511d61a9
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2019-01-25 23:14:50 -0500

    Get default for iniDate and EndDate in reports

--- src/main/java/com/soinsoftware/vissa/util/DateUtil.java
+++ src/main/java/com/soinsoftware/vissa/util/DateUtil.java
@@ -105,4 +105,20 @@ public class DateUtil {
 		return calendar.getTime();
 	}
 
+	
+	/**
+	 * Retorna la fecha inicial para reportes, hasta las 00:00:00
+	 * @return
+	 */
+	public static LocalDateTime getDefaultIniDate() {
+		return LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);		
+	}
+	
+	/**
+	 * Retorna la fecha fin para reportes, hasta las 23:59:59
+	 * @return
+	 */
+	public static LocalDateTime getDefaultEndDate() {
+		return LocalDateTime.now().withHour(23).withMinute(59).withSecond(59);		
+	}
 }
